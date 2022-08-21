f6eb431f0445b35fc34eb1e4d436109189659ab9
김태연 <tykim@ban-life.com>
2022-08-21 16:22:40 +0900

    client - 랜딩 페이지 TS로 변경

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import { Container, Grid } from '@material-ui/core';
-
 import Posts from '../Posts/Posts';
 
 function LandingPage() {
-
   const userName = window.localStorage.getItem('userName');
 
   return (
     <div>
       <header>
         <Navigation userName={userName} />
-
       </header>
       <main>
-        <Container maxWidth="md" >
+        <Container maxWidth="md">
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} >
+            <Grid item xs={12}>
               <Posts userName={userName} />
             </Grid>
           </Grid>
         </Container>
       </main>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
